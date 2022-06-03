f2aff2e6654eb089c8ed5e0511272fe9e81c2c38
deevakalpana <deevakalpana12.neo@gmail.com>
2022-06-03 16:23:31 +0530

    first commit

---
+++ Sample3.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+public class Sample3
+{       
+public static void main(String args[])
+{
+int arr[] = {1,6,2,4};
+Arrays.sort(arr);
+System.out.print(arr[arr.length-2]);
+}
+}
